4dd2c404280145d0e8863fb48f67c8438f2b3db7
TLNTLSS <maksscvorcov@mail.ru>
2025-03-03 17:37:51 +0300

    1

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -15,14 +15,35 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        //Кнопка для расчета
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Получение входных данных
+                double principal = double.Parse(PrincipalAmountTextBox.Text);
+
+                // Конвертация процента в долю
+                double interestRate = double.Parse(InterestRateTextBox.Text) / 100;
+                int years = int.Parse(YearsTextBox.Text);
+
+                // Расчет итоговой суммы
+                double totalAmount = principal * Math.Pow((1 + interestRate), years);
+
+                // Отображение результата и форматирование до 2 знаков после запятой
+                ResultTextBlock.Text = $"{totalAmount:F2} ₽";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Пожалуйста, введите корректные значения." + "\n" + ex.Message);
+            }
         }
     }
 }
